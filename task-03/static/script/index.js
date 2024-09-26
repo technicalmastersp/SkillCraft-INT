9f6dae188c0793d0d1578f2ae27d9e3786f2557a
technicalmastersp <takebestservices@gmail.com>
2024-09-27 01:02:07 +0530

    add sume new feture and alert function and fevicon image

--- task-03/static/script/index.js
+++ task-03/static/script/index.js
@@ -3,8 +3,21 @@ const resetButton = document.getElementById('resetBtn');
 let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let isGameActive = true;
+let i = 0;
 
 function start() {
+    function myFunction() {
+        swal("Good job!", "You clicked the button!", "success");
+        console.log("Function executed");
+    }
+    if (!sessionStorage.getItem("functionExecuted")) {
+        myFunction();
+        sessionStorage.setItem("functionExecuted", "true");
+    }
+    window.addEventListener("beforeunload", function() {
+        sessionStorage.removeItem("functionExecuted");
+    });
+    
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -35,19 +48,43 @@ function start() {
         for (const condition of winningConditions) {
             const [a, b, c] = condition;
             if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-                alert(`Player ${board[a]} wins!`);
+                swal({
+                    title: `Congratulation ${board[a]} wins!`,
+                    text: `Player ${board[a]} wins!`,
+                    imageUrl: "static/media/win.png"
+                });
                 isGameActive = false;
                 return;
             }
         }
     
         if (!board.includes('')) {
-            alert("It's a draw!");
+            swal({
+                title: "It's a draw!",
+                text: "Please restart game and play again!",
+                imageUrl: "static/media/draw.png"
+            });
             isGameActive = false;
         }
     };
     
     const resetGame = () => {
+        swal({
+            title: "Are you sure?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, Restart the Game!",
+            closeOnConfirm: false
+          },
+          function(){
+            swal({
+                title: "Restarted!",
+                text: "Your game has been restarted.",
+                type: "success",
+                timer: 1200
+            });
+          });
         board = ['', '', '', '', '', '', '', '', ''];
         isGameActive = true;
         currentPlayer = 'X';
